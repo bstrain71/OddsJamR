416ec04b9d34cd412d0aafc191f21b5dd42b0dc8
Brandon Strain <Brandon@Helens-MacBook-Pro.local>
2021-10-04 23:10:08 -0400

    Utils bug fix

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,11 @@
 find_best_line <- function(x){
+  x <- as.numeric(x)
   if(any(x > 0)){
     x <- x[x > 0]
     x <- max(x)
     return(x)
   } else {
-    x <- min(x)
+    x <- max(x)
     return(x)
   }
 }
